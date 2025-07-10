eea29e374709c93854e7fd7c710a3ef0b4a90681
Dimqe <petr.makarov249@gmail.com>
2025-07-10 18:51:32 +0200

    ts

---
+++ 01-ts-basics/src/task-6.ts
@@ -0,0 +1,9 @@
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+const firstNumber = getFirstElement<number>([1, 2, 3]);            
+const firstString = getFirstElement<string>(["a", "b", "c"]);     
+const firstBoolean = getFirstElement<boolean>([true, false, true]); 
+
+const mixed = getFirstElement([1, "two", true]); 
