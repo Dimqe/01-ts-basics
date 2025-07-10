a43b36358ad18370e52877c8cfd118b111438a52
Dimqe <petr.makarov249@gmail.com>
2025-07-10 21:01:22 +0200

    fixed

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,9 +2,8 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-
 const firstNumber = getFirstElement<number>([1, 2, 3]);           
 const firstString = getFirstElement<string>(["a", "b", "c"]);     
-const firstBoolean = getFirstElement<boolean>([true, false, true]); 
+const firstBoolean = getFirstElement<boolean>([true, false, true]);
 
-const mixed = getFirstElement([1, "two", true]); 
+const mixed = getFirstElement<number | string | boolean>([1, "two", true]);
